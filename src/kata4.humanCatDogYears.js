7aa374de951c7fff3bb992d15ecbaddaf2e975b4
Jenny Rafferty <raffertyjenny@gmail.com>
2023-03-11 18:00:53 +0000

    passed dog-cat years test

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,11 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  const remainingCatYears = (number - 2) * 4;
+  const AgeTwo = 24;
+  const catAge = remainingCatYears + AgeTwo;
+  const remainingDogYears = (number - 2) * 5;
+  const dogAge = remainingDogYears + AgeTwo;
+  const arr = [number, catAge, dogAge];
+  return arr;
+};
 
 module.exports = humanCatDogYears;
