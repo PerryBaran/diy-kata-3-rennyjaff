dfb2f23f952901524ae8c9cc27475d3749fea48d
Jenny Rafferty <raffertyjenny@gmail.com>
2023-03-11 20:20:53 +0000

    passed reachDestination test

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
-
+const reachDestination = (distance, speed) => {
+  const arrivalTime = distance / speed;
+  const timeRoundedUp = Math.round(arrivalTime * 2) / 2;
+  return `I should be there in ${timeRoundedUp} hours.`;
+}
 module.exports = reachDestination;
