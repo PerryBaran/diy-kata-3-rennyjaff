26eaa15138188b8c627f27cdb1afd61302c7d3f1
Jenny Rafferty <raffertyjenny@gmail.com>
2023-03-12 10:15:24 +0000

    passed employerRole test

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,7 +1,7 @@
 const { humanCatDogYears } = require("../src");
 
 describe("humanCatDogYears", () => {
-  it(" when passed a number, it returns an array of three numbers, the first number should be the human years passed as an argument, the second element is cat years, the third element is dog years", () => {
+  it("returns the age in human years, cat years and dog years", () => {
     expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
     expect(humanCatDogYears(20)).toEqual([20, 96, 114]);
   });
